4d38778b4027de2ed59a8a45531ff304016e6036
paytye <tripleee.p@gmail.com>
2019-10-16 16:49:10 +0700

    rewrite home page

--- klb_ui/src/pages/Home/Home.jsx
+++ klb_ui/src/pages/Home/Home.jsx
@@ -9,276 +9,239 @@ import PaginationButton from "../../components/Pagination.jsx";
 // import { FaPlusCircle } from "react-icons/fa";
 
 const Content = styled.div`
-	background-color: #f9f9f9;
-	padding: 75px;
-	color: #73777a;
+   background-color: #f9f9f9;
+   padding: 75px;
+   color: #73777a;
 `;
 
 const Headline = styled.h1`
-	font-weight: bold;
+   font-weight: bold;
 `;
 
 const ButtonNewPost = styled(Button)`
-	background-color: #fd7e47 !important;
-	border: none !important;
-	font-weight: bold !important;
-	border-radius: 25px !important;
-	padding-left: 15px !important;
-	padding-right: 15px !important;
+   background-color: #fd7e47 !important;
+   border: none !important;
+   font-weight: bold !important;
+   border-radius: 25px !important;
+   padding-left: 15px !important;
+   padding-right: 15px !important;
 `;
 
 const StyledPagination = styled(Pagination)`
-	background-color: transparent;
-	border: 0px !important;
-	margin-left: auto;
-	margin-right: auto;
+   background-color: transparent;
+   border: 0px !important;
+   margin-left: auto;
+   margin-right: auto;
 `;
 
 const StyledItem = styled(PaginationItem)`
-	padding: 0;
+   padding: 0;
 `;
 
 const StyledLink = styled(PaginationLink)`
-	box-shadow: none !important
-	// hover: {
-	// 	background-color: #b5c9d4 !important
-	// }
-`
+   box-shadow: none !important;
+   // hover: {
+   // 	background-color: #b5c9d4 !important
+   // }
+`;
 
 class NewPost extends React.Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			posts: [],
-			postsLength: 0,
-			currentPage: 1,
-			postsPerPage: 10,
-			pageLength: 1,
-			pageRangeDisplayed: 10
-		};
-	}
+   constructor(props) {
+      super(props);
+      this.state = {
+         posts: [],
+         postsLength: 0,
+         currentPage: 1,
+         postsPerPage: 10,
+         pageLength: 1,
+         pageRangeDisplayed: 10
+      };
+   }
 
-	componentDidMount() {
-		this.setState({ currentPage: Number(this.props.match.params.currentPage || 1) })
-		API.get(`/posts/count`).then(res => {
-			this.setState({ postsLength: res.data }, () => {
-				this.setState({
-					pageLength: Math.ceil(
-						this.state.postsLength / this.state.postsPerPage
-					)
-				});
-			});
-		});
+   componentDidMount() {
+      this.setState({
+         currentPage: Number(this.props.match.params.currentPage || 1)
+      });
 
-		const limit = this.state.postsPerPage;
-		const skip = 0;
+      API.get(`/posts/count`).then(res => {
+         this.setState({ postsLength: res.data }, () => {
+            this.setState({
+               pageLength: Math.ceil(
+                  this.state.postsLength / this.state.postsPerPage
+               )
+            });
+         });
+      });
 
-		API.get(`/posts`, { params: { limit, skip } }).then(res => {
-			const posts = res.data;
-			this.setState({ posts });
-		});
-	}
+      const limit = this.state.postsPerPage;
+      const skip = (this.state.currentPage - 1) * limit;
 
-	routeChange = () => {
-		console.log("route change", this.state.currentPage);
-		const path = `/page/${this.state.currentPage}`;
-		console.log(path);
-		this.props.history.push(path);
-	};
+      API.get(`/posts`, { params: { limit, skip } }).then(res => {
+         const posts = res.data;
+         this.setState({ posts });
+      });
+   }
 
-	first = () => {
-		this.setState(
-			{
-				currentPage: 1
-			},
-			() => {
-				const limit = this.state.postsPerPage;
-				const skip = (this.state.currentPage - 1) * limit;
+   componentDidUpdate(prevProps, prevState) {
+      if (prevState.currentPage !== this.state.currentPage) {
+         API.get(`/posts/count`).then(res => {
+            this.setState({ postsLength: res.data }, () => {
+               this.setState({
+                  pageLength: Math.ceil(
+                     this.state.postsLength / this.state.postsPerPage
+                  )
+               });
+            });
+         });
 
-				API.get(`/posts`, { params: { limit, skip } }).then(res => {
-					const posts = res.data;
-					this.setState({ posts });
-				});
-			}
-		);
-	};
+         const limit = this.state.postsPerPage;
+         const skip = (this.state.currentPage - 1) * limit;
 
-	previous = () => {
-		this.setState(
-			{
-				currentPage: this.state.currentPage - 1
-			},
-			() => {
-				const limit = this.state.postsPerPage;
-				const skip = (this.state.currentPage - 1) * limit;
+         API.get(`/posts`, { params: { limit, skip } }).then(res => {
+            const posts = res.data;
+            this.setState({ posts });
+         });
+      }
+   }
 
-				API.get(`/posts`, { params: { limit, skip } }).then(res => {
-					const posts = res.data;
-					this.setState({ posts });
-				});
+   first = () => {
+      this.setState({ currentPage: 1 }, () => {
+         this.routeChange();
+      });
+   };
 
-				this.routeChange()
-			}
-		);
-	};
+   previous = () => {
+      this.setState({ currentPage: this.state.currentPage - 1 }, () => {
+         this.routeChange();
+      });
+   };
 
-	next = () => {
-		this.setState(
-			{
-				currentPage: this.state.currentPage + 1
-			},
-			() => {
-				const limit = this.state.postsPerPage;
-				const skip = (this.state.currentPage - 1) * limit;
+   next = () => {
+      this.setState({ currentPage: this.state.currentPage + 1 }, () => {
+         this.routeChange();
+      });
+   };
 
-				API.get(`/posts`, { params: { limit, skip } }).then(res => {
-					const posts = res.data;
-					this.setState({ posts });
-				});
+   last = () => {
+      this.setState({ currentPage: this.state.pageLength }, () => {
+         this.routeChange();
+      });
+   };
 
-				this.routeChange()
-			}
-		);
-	};
+   handleClick = event => {
+      this.setState({ currentPage: Number(event.target.id) }, () => {
+         this.routeChange();
+      });
+   };
 
-	last = () => {
-		this.setState(
-			{
-				currentPage: this.state.pageLength
-			},
-			() => {
-				const limit = this.state.postsPerPage;
-				const skip = (this.state.currentPage - 1) * limit;
+   routeChange = () => {
+      const path = `/page/${this.state.currentPage}`;
+      this.props.history.push(path);
+   };
 
-				API.get(`/posts`, { params: { limit, skip } }).then(res => {
-					const posts = res.data;
-					this.setState({ posts });
-				});
+   routeNewPost = () => {
+      const path = `/newpost`;
+      this.props.history.push(path);
+   };
 
-				this.routeChange()
-			}
-		);
-	};
+   render() {
+      const disableButtom = { leftButton: false, rightButton: false };
+      const { posts, currentPage, pageLength, pageRangeDisplayed } = this.state;
 
-	handleClick = event => {
-		this.setState({ currentPage: Number(event.target.id) }, () => {
-			const limit = this.state.postsPerPage;
-			const skip = (this.state.currentPage - 1) * limit;
+      const renderPost = posts.map((post, index) => {
+         return <PostHome key={index} post={post} />;
+      });
 
-			API.get(`/posts`, { params: { limit, skip } }).then(res => {
-				const posts = res.data;
-				this.setState({ posts });
-			});
+      const pageNumbers = [];
 
-			this.routeChange()
-		});
-	};
+      const calLastPageNum =
+         Math.floor((currentPage - 1) / pageRangeDisplayed) *
+            pageRangeDisplayed +
+         pageRangeDisplayed;
+      const firstPageNum =
+         Math.floor((currentPage - 1) / pageRangeDisplayed) *
+            pageRangeDisplayed +
+         1;
+      const lastPageNum =
+         calLastPageNum > pageLength ? pageLength : calLastPageNum;
 
-	routeNewPost = () => {
-		const path = `/newpost`;
-		this.props.history.push(path);
-	};
+      for (let i = firstPageNum; i < lastPageNum + 1; i++) {
+         pageNumbers.push(i);
+      }
 
-	render() {
-		const disableButtom = { leftButton: false, rightButton: false };
-		const { posts, currentPage, pageLength, pageRangeDisplayed } = this.state;
+      const renderPageNumbers = pageNumbers.map((number, index) => {
+         return (
+            <PaginationButton
+               handleClick={this.handleClick}
+               key={index}
+               number={number}
+               currentPage={currentPage}
+            />
+         );
+      });
 
-		console.log("currentPage", currentPage); //////////////////////
-		
-		const renderPost = posts.map((post, index) => {
-			return <PostHome key={index} post={post} />;
-		});
+      disableButtom.leftButton = currentPage === 1 ? true : false;
+      disableButtom.rightButton = currentPage === pageLength ? true : false;
 
-		const pageNumbers = [];
+      return (
+         <Content>
+            <Row>
+               <Col xs={7} className="mx-auto my-0">
+                  <Row>
+                     <Col>
+                        <Headline>Post</Headline>
+                     </Col>
+                     <Col>
+                        <ButtonNewPost
+                           className="float-right"
+                           onClick={this.routeNewPost}
+                        >
+                           New Post
+                        </ButtonNewPost>
+                     </Col>
+                  </Row>
+                  <div
+                     style={{
+                        borderBottom: "1px solid #b8b8b8",
+                        marginBottom: "50px"
+                     }}
+                  >
+                     {renderPost}
+                  </div>
 
-		const calLastPageNum =
-			Math.floor((currentPage - 1) / pageRangeDisplayed) *
-				pageRangeDisplayed +
-			pageRangeDisplayed;
-		const firstPageNum =
-			Math.floor((currentPage - 1) / pageRangeDisplayed) *
-				pageRangeDisplayed +
-			1;
-		const lastPageNum =
-			calLastPageNum > pageLength ? pageLength : calLastPageNum;
-
-		for (let i = firstPageNum; i < lastPageNum + 1; i++) {
-			pageNumbers.push(i);
-		}
-
-		const renderPageNumbers = pageNumbers.map((number, index) => {
-			return (
-				<PaginationButton
-					handleClick={this.handleClick}
-					key={index}
-					number={number}
-					currentPage={currentPage}
-				/>
-			);
-		});
-
-		disableButtom.leftButton = currentPage === 1 ? true : false;
-		disableButtom.rightButton = currentPage === pageLength ? true : false;
-
-		return (
-			<Content>
-				<Row>
-					<Col xs={7} className="mx-auto my-0">
-						<Row>
-							<Col>
-								<Headline>Post</Headline>
-							</Col>
-							<Col>
-								<ButtonNewPost
-									className="float-right"
-									onClick={this.routeNewPost}
-								>
-									New Post
-								</ButtonNewPost>
-							</Col>
-						</Row>
-						<div
-							style={{
-								borderBottom: "1px solid #b8b8b8",
-								marginBottom: "50px"
-							}}
-						>
-							{renderPost}
-						</div>
-
-						<Container
-							fluid
-							style={{
-								marginLeft: "auto",
-								marginRight: "auto",
-								width: "auto"
-							}}
-						>
-							<Row>
-								<Col  className="text-center" style={{ padding: "auto" }}>
-									<StyledPagination>
-										<StyledItem disabled={disableButtom.leftButton}>
-											<StyledLink first onClick={this.first} />
-										</StyledItem>
-										<StyledItem disabled={disableButtom.leftButton}>
-											<StyledLink previous onClick={this.previous} />
-										</StyledItem>
-										{renderPageNumbers}
-										<StyledItem disabled={disableButtom.rightButton}>
-											<StyledLink next onClick={this.next} />
-										</StyledItem>
-										<StyledItem disabled={disableButtom.rightButton}>
-											<StyledLink last onClick={this.last} />
-										</StyledItem>
-									</StyledPagination>
-								</Col>
-							</Row>
-						</Container>
-					</Col>
-				</Row>
-			</Content>
-		);
-	}
+                  <Container
+                     fluid
+                     style={{
+                        marginLeft: "auto",
+                        marginRight: "auto",
+                        width: "auto"
+                     }}
+                  >
+                     <Row>
+                        <Col>
+                           <StyledPagination className="d-flex justify-content-center">
+                              <StyledItem disabled={disableButtom.leftButton}>
+                                 <StyledLink first onClick={this.first} />
+                              </StyledItem>
+                              <StyledItem disabled={disableButtom.leftButton}>
+                                 <StyledLink previous onClick={this.previous} />
+                              </StyledItem>
+                              {renderPageNumbers}
+                              <StyledItem disabled={disableButtom.rightButton}>
+                                 <StyledLink next onClick={this.next} />
+                              </StyledItem>
+                              <StyledItem disabled={disableButtom.rightButton}>
+                                 <StyledLink last onClick={this.last} />
+                              </StyledItem>
+                           </StyledPagination>
+                        </Col>
+                     </Row>
+                  </Container>
+               </Col>
+            </Row>
+         </Content>
+      );
+   }
 }
 
 export default NewPost;
