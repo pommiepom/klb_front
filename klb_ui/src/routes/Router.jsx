11eb89bf4fdd4ebf0130fa72d143352faf1451c2
paytye <tripleee.p@gmail.com>
2019-10-21 17:23:43 +0700

    search

--- klb_ui/src/routes/Router.jsx
+++ klb_ui/src/routes/Router.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Switch, Route } from "react-router-dom";
+import { Switch, Route, Link } from "react-router-dom";
 
 import Home from "../pages/Home/Home.jsx";
 import Post from "../pages/Post/Post.jsx";
@@ -9,19 +9,22 @@ import NewPost from "../pages/NewPost/NewPost.jsx";
 import Profile from "../pages/Profile/Profile.jsx";
 
 class Router extends React.Component {
-	render() {
-		return (
-			<Switch>
-				{/* <Route exact path="/page/1" component={Home}></Route> */}
-				<Route exact path="/profile" component={Profile}></Route>
-				<Route exact path="/page/:currentPage" component={Home}></Route>
-				<Route exact path="/newpost" component={NewPost}></Route>
-				<Route exact path="/search/:params" component={SearchResult}></Route>
+   render() {
+      return (
+         <Switch>
+            {/* <Route exact path="/page/1" component={Home}></Route> */}
+            <Route exact path="/profile" component={Profile}></Route>
+            <Route exact path="/page/:currentPage" component={Home}></Route>
+            <Route exact path="/newpost" component={NewPost}></Route>
+            <Route exact path="/search/title=:title&category=:category&fromUser=:fromUser&fromDate=:fromDate&toDate=:toDate"					
+               component={SearchResult}
+            ></Route>
 				<Route exact path="/search" component={Search}></Route>
-				<Route exact path="/post/:id" component={Post}></Route>
-			</Switch>
-		);
-	}
+            <Route exact path="/post/:id" component={Post}></Route>
+            <Route exact path="/" component={Home}></Route>
+         </Switch>
+      );
+   }
 }
 
 export default Router;
